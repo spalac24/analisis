9b24a47f698f80d7d6425889602b4389e66930e1
Santiago Palacio <spalac24@gmail.com>
2013-08-27 01:30:28 -0500

    testing custom plotting

--- test_2.py
+++ test_2.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
 
-print "please enter the function, remember to use ** for exponentiation, and for mathematical functions, prefixthem with np (e.g. np.cos(x) instead of cos(x)"
+print "please enter the function, remember to use ** for exponentiation, and for mathematical functions, prefixthem with np (e.g. np.cos(x) instead of cos(x))"
 fun = raw_input()
 print "please enter lower and upper bound for graphics"
 lo = float(input())
